fa138d8e66ff7d4589d312c99a23b45ed44915b5
frisokulk <frisokulk@lostlemon.nl>
2018-08-25 10:16:12 +0200

    Basic project structure

---
+++ src/main/java/nl/kulk/reactivemoviesrest/ReactiveMoviesRestApplication.java
@@ -0,0 +1,12 @@
+package nl.kulk.reactivemoviesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMoviesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMoviesRestApplication.class, args);
+	}
+}
